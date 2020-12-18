154e39a4d9d96edd70ce4dce6e2475de155453bc
maksimus <gorlovmaks23@gmail.com>
2020-12-18 23:55:06 +0600

    Доавила функции для администратора приложения

---
+++ src/store/actions/adminActions.js
@@ -0,0 +1,59 @@
+import axios from "../../axiosApi";
+import {FETCH_COCKTAILS_SUCCESS, FETCH_FAILURE} from "../actionTypes";
+
+const fetchAllCocktailsSuccess = cocktails => {
+    return {type: FETCH_COCKTAILS_SUCCESS, cocktails};
+};
+
+const fetchFailure = error => {
+    return {type: FETCH_FAILURE, error};
+};
+
+export const fetchAllCocktails = () => {
+    return async dispatch => {
+        try {
+            await axios.get("/moderation/cocktails").then(response => {
+                dispatch(fetchAllCocktailsSuccess(response.data));
+            })}catch(e) {
+            if (e.response && e.response.data) {
+                dispatch(fetchFailure(e.response.data));
+            } else {
+                dispatch(fetchFailure({global: "No internet"}));
+            }
+        }
+    };
+};
+
+export const publish = (id) => {
+    return async (dispatch, getState) => {
+        try {
+            const token = getState().users.user.token;
+            const headers = {'Authorization': token};
+            await axios.put(`/moderation/cocktails/${id}`, {}, {headers});
+            dispatch(fetchAllCocktails());
+        } catch(e) {
+            if (e.response && e.response.data) {
+                dispatch(fetchFailure(e.response.data));
+            } else {
+                dispatch(fetchFailure({global: "No internet"}));
+            }
+        }
+    };
+};
+
+export const deleteItem = (id) => {
+    return async (dispatch, getState) => {
+        try {
+            const token = getState().users.user.token;
+            const headers = {'Authorization': token};
+            await axios.delete(`/moderation/cocktails/${id}`, {headers});
+            dispatch(fetchAllCocktails());
+        } catch(e) {
+            if (e.response && e.response.data) {
+                dispatch(fetchFailure(e.response.data));
+            } else {
+                dispatch(fetchFailure({global: "No internet"}));
+            }
+        }
+    };
+};
