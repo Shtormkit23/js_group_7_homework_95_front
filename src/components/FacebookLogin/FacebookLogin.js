509ed06189a71e9d60241794ed01a5994fb1f388
maksimus <gorlovmaks23@gmail.com>
2020-12-18 23:54:44 +0600

    Компонент для входа через фб

---
+++ src/components/FacebookLogin/FacebookLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import FacebookLoginButton from 'react-facebook-login/dist/facebook-login-render-props';
+import {Button} from "@material-ui/core";
+import {useDispatch} from "react-redux";
+import {fbAppId} from "../../constants";
+import {facebookLogin} from "../../store/actions/usersActions";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    submit: {
+        width: "300px",
+        backgroundColor: "#808080"
+    }
+}));
+
+const FacebookLogin = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const facebookResponse = response => {
+        if (response.id) {
+            dispatch(facebookLogin(response));
+        }
+    };
+
+    return <FacebookLoginButton
+        appId={fbAppId}
+        fields="name,email,picture"
+        render={renderProps => (
+            <Button
+                onClick={renderProps.onClick}
+                type="submit"
+                fullWidth
+                variant="contained"
+                className={classes.submit}
+            >
+                Enter with Facebook
+            </Button>
+        )}
+        callback={facebookResponse}
+    />
+}
+
+export default FacebookLogin;
